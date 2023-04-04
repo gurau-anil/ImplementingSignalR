d11627c16ac8196ad6a245888679c836d73c28fd
Anil Gurau <gurau.anil@gmail.com>
2023-04-05 01:13:09 +0545

    installing signalR Client and configuring the connection

--- ImplementingSignalR/wwwroot/js/site.js
+++ ImplementingSignalR/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/messageHub")
+.configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log('connected');
+       
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+       
+    }
+   
+};
+connection.onclose(async () => {
+    await start();
+   
+});
+start();
