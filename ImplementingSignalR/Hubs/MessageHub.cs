80c8cd0d229b06f2dac0ed535239581b860e9c61
Anil Gurau <gurau.anil@gmail.com>
2023-04-08 21:33:27 +0545

    Sending message to connected clients other than yourself

--- ImplementingSignalR/Hubs/MessageHub.cs
+++ ImplementingSignalR/Hubs/MessageHub.cs
@@ -12,7 +12,11 @@ namespace ImplementingSignalR.Hubs
         {
             //Sending Message to all clients. 'ReceiveMessage' method can be invoked in frontend to receive the message sent from here. 
             // Note: The method name should exactly match to get the message sent from here.
-            await Clients.All.SendAsync("ReceiveMessage",message);
+            //await Clients.All.SendAsync("ReceiveMessage",message);
+
+
+            //Sending message to connected clients other than yourself
+            await Clients.Others.SendAsync("ReceiveMessage",message);
         }
 
         public override async Task OnConnectedAsync()
